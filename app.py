6469b3b611743d08a3ddaecd878d803fbf036f40
vedantbacche-boop <vedant.bacche@gmail.com>
2025-10-22 18:37:38 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -51,9 +51,17 @@ def index():
 if __name__ == "__main__":
     app.run(debug=True)
 
-    import os
+    from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Flask on Render!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
